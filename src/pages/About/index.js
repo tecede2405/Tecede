983f73b676f2cb98900227f6f732dd9aaf7a4384
Tecede <thoaixd123@gmail.com>
2024-12-24 18:29:30 +0700

    update

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,7 @@
 import Image from '../../img/AboutIamge.png';
 import Tabbar from '../../component/tabar';
 import zhongli from '../../img/zhongli.png';
+import meme from '../../img/meme.png';
 function About() {
     return (
         <>
@@ -19,9 +20,9 @@ function About() {
                     <div className="about__title">
                         <p className="about__produce--desc">"Có thể sẽ có bạn thắc mắc cái tên Thoại Xayda và biệt danh "Tecede" có ý nghĩa gì đúng không?"</p>
                         <p>Thực ra tên Thoại Xayda cũng chỉ được đặt ngẫu hứng từ nhiều năm về trước được lấy cảm hứng từ tên 1 chủng tộc trong phim 7 viên ngọc rồng.
-                        Còn biệt danh "Tecede" từ những kí tự đầu của cái tên đó mà ra. Te là 'T', ce là 'X' và de là "D".</p>
+                        Còn biệt danh "Tecede" từ những kí tự đầu của cái tên đó mà ra. Te là 'T', ce là 'X' và de là 'D'.</p>
                     </div>
-                    <img className="about__display" src="https://i.pinimg.com/236x/ed/3b/8d/ed3b8d9dd0c84d11832528e2f339d8ea.jpg" alt="" />
+                    <img className="about__display" src={ meme } alt="" />
                 </div>
             </div>
            </div>
