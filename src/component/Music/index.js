d0616c4e2b58c0b26763f15969caed9c6014f32d
Tecede <thoaixd123@gmail.com>
2024-12-25 19:12:38 +0700

    update

--- src/component/Music/index.js
+++ src/component/Music/index.js
@@ -1,27 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import nhactet from '../../audio/nhactet.mp3';
+import lofi from '../../audio/lofi.mp3';
+import merry from '../../audio/merry.mp3';
+import nhacTrung from '../../audio/nhacTrung.mp3';
+const songs = [nhactet, lofi, merry, nhacTrung];
 
 const BackgroundMusic = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
 
   useEffect(() => {
     const audio = document.getElementById('background-music');
-    if (isPlaying) {
+    if (isPlaying && currentSong) {
       audio.play();
     } else {
       audio.pause();
     }
-  }, [isPlaying]);
+
+    const handleSongEnd = () => {
+      const randomSong = songs[Math.floor(Math.random() * songs.length)];
+      setCurrentSong(randomSong);
+      audio.play();
+    };
+
+    audio.addEventListener('ended', handleSongEnd);
+
+    return () => {
+      audio.removeEventListener('ended', handleSongEnd);
+    };
+  }, [isPlaying, currentSong]);
 
   const togglePlay = () => {
+    if (!isPlaying) {
+      const randomSong = songs[Math.floor(Math.random() * songs.length)];
+      setCurrentSong(randomSong);
+    }
     setIsPlaying(!isPlaying);
   };
 
   return (
     <div>
       <audio id="background-music" loop className="hidden-audio">
-        <source src={ nhactet } type="audio/mpeg" />
-        Your browser does not support the audio tag.
+        {currentSong && <source src={currentSong} type="audio/mpeg" />}
       </audio>
       <button onClick={togglePlay} className="toggle-button">
         {isPlaying ? 'Tắt nhạc' : 'Bật nhạc'}
@@ -31,3 +51,4 @@ const BackgroundMusic = () => {
 }
 
 export default BackgroundMusic;
+
