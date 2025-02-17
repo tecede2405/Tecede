bf49ff5464d856d936317af75599ec861303e516
Tecede <thoaixd123@gmail.com>
2025-02-17 17:54:39 +0700

    update

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -5,17 +5,7 @@ function Contact() {
            <div className="contact">
             <Tabbar />
             <div className="contact__content">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVW0M3bVFC9P9duBxMJD3DOkdGY0bSDLd6kKcFMSw6ViAJRg/viewform?embedded=true" 
-                width="100%" 
-                height="880px"
-                style={{ margin: '30px 0 0 0' }} 
-                frameborder="0"
-                scrolling="no"
-                marginheight="0" 
-                marginwidth="0"
-                >
-                Đang tải…
-                </iframe>
+               <p className="contact__title">Chưa hoàn thành</p>
             </div> 
            </div>
         </>
