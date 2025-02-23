d41a43a05e4e4ece615d2ce46e2673c8b8014d80
Tecede <thoaixd123@gmail.com>
2025-02-23 16:12:30 +0700

    update

--- src/component/tabar/index.js
+++ src/component/tabar/index.js
@@ -6,10 +6,10 @@ function Tabbar(){
         <div className="tabbar">
                 <p>My Channels</p>
                 <ul>
-                    <li><a href="https://www.facebook.com/thoaidz.vx" target="_blank"><FaFacebook class="icon"/>FaceBook</a></li>
-                    <li><a href="https://www.youtube.com/@Tecede2405" target="_blank"><FaYoutube class="icon"/> Youtube</a></li>
-                    <li><a href="https://www.tiktok.com/@p.qthoai2405_" target="_blank"><FaTiktok class="icon"/> Tiktok</a></li>
-                    <li><a href="https://www.instagram.com/p.qthoai2405_/" target="_blank"><FaInstagram class="icon"/> Instagram</a></li>
+                    <li><a href="https://www.facebook.com/thoaidz.vx" target="_blank" rel="noopener noreferrer"><FaFacebook class="icon"/>FaceBook</a></li>
+                    <li><a href="https://www.youtube.com/@Tecede2405" target="_blank" rel="noopener noreferrer"><FaYoutube class="icon"/> Youtube</a></li>
+                    <li><a href="https://www.tiktok.com/@p.qthoai2405_" target="_blank" rel="noopener noreferrer"><FaTiktok class="icon"/> Tiktok</a></li>
+                    <li><a href="https://www.instagram.com/p.qthoai2405_/" target="_blank" rel="noopener noreferrer"><FaInstagram class="icon"/> Instagram</a></li>
                     
                 </ul>
             </div>
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -5,7 +5,7 @@ function Contact() {
            <div className="contact">
             <Tabbar />
             <div className="contact__content">
-               <p className="contact__title">Chưa hoàn thành</p>
+               <p className="contact__title">Chưa hoàn thành.<br />Trang sẽ liên tục được cập nhật.</p>
             </div> 
            </div>
         </>
