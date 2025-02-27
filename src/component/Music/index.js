33eaa76e065ed4978696bbf34c0debae80bd5005
Tecede <thoaixd123@gmail.com>
2025-02-27 16:46:04 +0700

    update video

--- src/component/Music/index.js
+++ src/component/Music/index.js
@@ -17,52 +17,50 @@ import music15 from '../../audio/Tùng TeA - Già Cùng Nhau Là Được ft. PC
 import music16 from '../../audio/Âm Thầm Bên Em.mp3';
 import music17 from '../../audio/Đom Đóm - Jack [Lyrics video].mp3';
 
-const songs = [music1,music2,music3,music4,music5,music6,music7,music8,music8,music9,music10,music11,music12,music13,music14,music15,music16,music17];
+const allSongs = [music1, music2, music3, music4, music5, music6, music7, music8, music9, music10, music11, music12, music13, music14, music15, music16, music17];
+
+const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
 
 const BackgroundMusic = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [currentSong, setCurrentSong] = useState(null);
+  const [playlist, setPlaylist] = useState(shuffleArray([...allSongs]));
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const audio = document.getElementById('background-music');
-    if (isPlaying && currentSong) {
+    if (isPlaying) {
       audio.play();
     } else {
       audio.pause();
     }
 
     const handleSongEnd = () => {
-      const randomSong = songs[Math.floor(Math.random() * songs.length)];
-      setCurrentSong(randomSong);
-      audio.play();
+      if (currentIndex < playlist.length - 1) {
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        setPlaylist(shuffleArray([...allSongs]));
+        setCurrentIndex(0);
+      }
     };
 
     audio.addEventListener('ended', handleSongEnd);
-
     return () => {
       audio.removeEventListener('ended', handleSongEnd);
     };
-  }, [isPlaying, currentSong]);
+  }, [isPlaying, currentIndex, playlist]);
 
   const togglePlay = () => {
-    if (!isPlaying) {
-      const randomSong = songs[Math.floor(Math.random() * songs.length)];
-      setCurrentSong(randomSong);
-    }
     setIsPlaying(!isPlaying);
   };
 
   return (
     <div>
-      <audio id="background-music" loop className="hidden-audio">
-        {currentSong && <source src={currentSong} type="audio/mpeg" />}
-      </audio>
+      <audio id="background-music" src={playlist[currentIndex]} autoPlay={isPlaying} />
       <button onClick={togglePlay} className="toggle-button">
         {isPlaying ? 'Tắt nhạc' : 'Bật nhạc'}
       </button>
     </div>
   );
-}
+};
 
 export default BackgroundMusic;
-
