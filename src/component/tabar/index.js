9249150078f696baf1e5f920722d0525d65bf877
Tecede <thoaixd123@gmail.com>
2025-03-11 18:23:38 +0700

    update

--- src/component/tabar/index.js
+++ src/component/tabar/index.js
@@ -8,7 +8,7 @@ function Tabbar(){
                 <ul>
                     <li><a href="https://www.facebook.com/thoaidz.vx" target="_blank" rel="noopener noreferrer"><FaFacebook class="icon"/>FaceBook</a></li>
                     <li><a href="https://www.youtube.com/@Tecede2405" target="_blank" rel="noopener noreferrer"><FaYoutube class="icon"/> Youtube</a></li>
-                    <li><a href="https://www.tiktok.com/@p.qthoai2405_" target="_blank" rel="noopener noreferrer"><FaTiktok class="icon"/> Tiktok</a></li>
+                    <li><a href="https://www.tiktok.com/@tecede24.5" target="_blank" rel="noopener noreferrer"><FaTiktok class="icon"/> Tiktok</a></li>
                     <li><a href="https://www.instagram.com/p.qthoai2405_/" target="_blank" rel="noopener noreferrer"><FaInstagram class="icon"/> Instagram</a></li>
                     
                 </ul>
