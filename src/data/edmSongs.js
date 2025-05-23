16656e2626fe086f72ac119a322ecffb40826faf
Tecede <thoaixd123@gmail.com>
2025-05-24 00:59:29 +0700

    devide files

---
+++ src/data/edmSongs.js
@@ -0,0 +1,106 @@
+import music1 from '../audio/NhacEDM/Ahrix - Nova.mp3';
+import music2 from '../audio/NhacEDM/Legends Never Die (ft. Against The Current)  Worlds 2017 - League of Legends.mp3';
+import music3 from '../audio/NhacEDM/TheFatRat - Never Be Alone.mp3';
+import music4 from '../audio/NhacEDM/Alan Walker - Alone.mp3';
+import music5 from '../audio/NhacEDM/TheFatRat - Rise Up.mp3';
+import music6 from '../audio/NhacEDM/Warriyo - Mortals (feat. Laura Brehm)  Future Trap  NCS - Copyright Free Music.mp3';
+import music7 from '../audio/NhacEDM/Ampyx - Holo.mp3';
+import music8 from '../audio/NhacEDM/Where We Started - Lost Sky ft. Jex (Lyrics  Vietsub).mp3';
+import music9 from '../audio/NhacEDM/Jim Yosef - Firefly  Progressive House  NCS - Copyright Free Music.mp3';
+import music10 from '../audio/NhacEDM/Tobu & Syndec - Dusk (Radio Edit).mp3';
+import music11 from '../audio/NhacEDM/DEAF KEV - Safe & Sound with Sendi Hoxha  House  NCS - Copyright Free Music.mp3';
+import music12 from '../audio/NhacEDM/Syn Cole - Time  Future House  NCS - Copyright Free Music.mp3';
+import music13 from '../audio/NhacEDM/cant wait - Jim Yosef.mp3';
+import music14 from '../audio/NhacEDM/Axel Johansson - Miracles (Lyrics) ft. Tina Stachowiak.mp3';
+
+ const songs = [
+    {
+      title: 'Miracles',
+      artist: 'Axel Johansson',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.3NfLjA_Msp7nANO6ugNV3QHaHa&pid=Api&P=0&h=180',
+      file: music14,
+    },
+    {
+      title: 'Cant Wait',
+      artist: 'Jim Yosef',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.BrMeIz4T8ikA-eqIiPLcOgHaEK&pid=Api&P=0&h=180',
+      file: music13,
+    },
+    {
+      title: 'Time',
+      artist: 'Syn Cole',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.pO6d2qtanK9Qy9s2_WGyNwHaHa&pid=Api&P=0&h=180',
+      file: music12,
+    },
+    {
+      title: 'Safe & Sound with Sendi Hoxha',
+      artist: 'DEAF KEV',
+      image: 'https://tse1.mm.bing.net/th?id=OIP.r1-VxQrjLjnJNxSDPJCXmwHaEK&pid=Api&P=0&h=180',
+      file: music11,
+    },
+    {
+      title: 'Nova',
+      artist: 'Ahrix',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.Z9d57V4cTSv3LuuKElJboQHaEK&pid=Api&P=0&h=180',
+      file: music1,
+    },
+    {
+      title: 'Legends Never Die',
+      artist: 'ft. Against The Current',
+      image: 'https://tse2.mm.bing.net/th?id=OIP.b9GU1HhlZLEqsbYYVjMiPwHaEK&pid=Api&P=0&h=180',
+      file: music2,
+    },
+    {
+      title: 'Never be alone',
+      artist: 'TheFatRat',
+      image: 'https://tse2.mm.bing.net/th?id=OIP.uCXj9DR5jQs6vtKyfN6NAAHaEK&pid=Api&P=0&h=180',
+      file: music3,
+    },
+    {
+      title: 'Alone',
+      artist: 'Alan Walker',
+      image: 'https://tse1.mm.bing.net/th?id=OIP.86og3_3k2N4m68K-hc1kygHaHa&pid=Api&P=0&h=180',
+      file: music4,
+    },
+    {
+      title: 'Rise up',
+      artist: 'The Fat Rat',
+      image: 'https://tse1.mm.bing.net/th?id=OIP.BuWu7EBn0k-6ONt01GlwPAHaHa&pid=Api&P=0&h=180',
+      file: music5,
+    },
+    {
+      title: 'Mortals ',
+      artist: 'Warriyo',
+      image: 'https://tse2.mm.bing.net/th?id=OIP.fmB_vgxfsl00dRDA27U9GQHaEK&pid=Api&P=0&h=180',
+      file: music6,
+    },
+    {
+      title: 'Holo',
+      artist: 'Ampyx',
+      image: 'https://tse1.mm.bing.net/th?id=OIP.-kY6fYOdbtoq8UE7hQJ5aAAAAA&pid=Api&P=0&h=180',
+      file: music7,
+    },
+    {
+      title: 'Where we started',
+      artist: 'Lost Sky ft. Jex',
+      image: 'https://tse1.mm.bing.net/th?id=OIP.9YY82U4QzaqHQSGsi639LAHaEK&pid=Api&P=0&h=180',
+      file: music8,
+    },
+    {
+      title: 'Firefly',
+      artist: 'Jim Yosef',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.G3zdnaOWM8vw_5xHONr4RwHaEK&pid=Api&P=0&h=180',
+      file: music9,
+    },
+    {
+      title: 'Dusk',
+      artist: 'Tobu & Syndec',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.ue2x6WGwV9TEDhOk5zvgKQHaEK&pid=Api&P=0&h=180',
+      file: music10,
+    },
+
+
+    // Thêm bài hát vào đây
+  ];
+
+export default songs;
