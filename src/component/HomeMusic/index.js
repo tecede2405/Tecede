3291ad04e33221fb20700933485e3d63ba471b54
Tecede <thoaixd123@gmail.com>
2025-05-28 23:18:01 +0700

    update thumbnail

--- src/component/HomeMusic/index.js
+++ src/component/HomeMusic/index.js
@@ -1,18 +1,18 @@
 import { useNavigate } from "react-router-dom"; 
 
 import "./homemusic.scss";
-import vpop from "../../img/music-thumnail/vpop.png";
-import usuk from "../../img/music-thumnail/usuk.png";
-import china from "../../img/music-thumnail/china.png";
-import edm from "../../img/music-thumnail/edm.png";
-import sad from "../../img/music-thumnail/sad.png";
+import nhactre from "../../img/musics-thumbnail/nhactre.png";
+import usuk from "../../img/musics-thumbnail/usuk.png";
+import china from "../../img/musics-thumbnail/china.png";
+import edm from "../../img/musics-thumbnail/edm.png";
+import mood from "../../img/musics-thumbnail/mood.png";
 import 'animate.css';
 const musicData = [
-    { title: "Nhạc Việt Nam", image: vpop, path: "nhac-tre",desc: "Nhạc vui buồn lẫn lộn,cực kì đa dạng thể loại từ thời tối cổ đến hiện đại." },
-    { title: "Nhạc Âu Mĩ", image: usuk, path: "usuk",desc: "Khi âm nhạc là liều thuốc chữa lành, nghe bao chill." },
-    { title: "Nhạc Trung Quốc", image: china , path: "trung-quoc",desc: "Giai điệu và lời nhạc cực kì hay dù nghe chả hiểu gì." },
-    { title: "Nhạc EDM", image: edm, path: "edm",desc: "Nhạc điện tử sôi động, cực kì thích hợp để nghe khi chơi thể thao,..." },
-    { title: "Nhạc chill", image: sad, path: "mood",desc: "Nhạc không lời giúp bạn thư giãn và tập trung hơn trong công việc." },
+    { image: nhactre, path: "nhac-tre"},
+    { image: usuk, path: "usuk"},
+    {image: china , path: "trung-quoc"},
+    {image: edm, path: "edm"},
+    {image: mood, path: "mood"},
 ];
 
 function HomeMusic() {
@@ -23,7 +23,7 @@ function HomeMusic() {
     <>
         <div>
             <h2 className="music-box-title">Trạm phát nhạc</h2>
-            <p className="music-box-desc">"Click vào để xem chi tiết"</p>
+            <p className="music-box-desc">"Nhạc sẽ luôn được cập nhật thường xuyên. <br />Click vào để xem chi tiết"</p>
         </div>
         <div className="container">
             <div className="row row-card">
@@ -32,13 +32,10 @@ function HomeMusic() {
                  data-wow-duration="1.5s" data-wow-delay="0.3s" key={index} onClick={() => navigate(`/music/${item.path}`)}>
                     <div className="card">
                         <img src={item.image} className="card-img-top" alt={item.title} />
-                        <div className="card-body">
-                            <h5 className="card-title">{item.title}</h5>
-                            <p className="card-desc">{item.desc}</p>
-                        </div>
+                        
                     </div>
                 </div>
-                ))}
+                ))}    
             </div>
         </div>
     </>
