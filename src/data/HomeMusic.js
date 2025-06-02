67c490dd1b8ecd8caca289b3d5aaac6a4c0b2d3c
Tecede <thoaixd123@gmail.com>
2025-06-02 19:35:54 +0700

    create file data

---
+++ src/data/HomeMusic.js
@@ -0,0 +1,44 @@
+import music1 from '../audio/NhacViet/[Lyrics] Yêu Thương Ngày Đó - Soobin Hoàng Sơn  OST Yêu Em Bất Chấp.mp3';
+import music2 from '../audio/NhacViet/Bình Yên Nơi Đâu  Sơn Tùng M-TP.mp3';
+import music3 from '../audio/NhacViet/Chi Pu  ANH ƠI Ở LẠI - Official MV (Chuyện Cám Tấm) (치푸).mp3';
+import music4 from '../audio/NhacViet/CÒN YÊU, ĐÂU AI RỜI ĐI - ĐỨC PHÚC  OFFICIAL MV.mp3';
+import music5 from '../audio/NhacViet/Dù cho tận thế.mp3';
+import music6 from '../audio/NhacViet/GHÉ QUA   OFFICIAL MV  Dick x PC x Tofu.mp3';
+import music7 from '../audio/NhacViet/HOA VÔ SẮC  ICM x JACK  OFFICIAL MUSIC VIDEO.mp3';
+import music8 from '../audio/NhacViet/HOAPROX x XESI - VÔ TÌNH  Official Lyric Video.mp3';
+import music9 from '../audio/NhacViet/HOÀNG TÔN - YÊU EM RẤT NHIỀU (Lyrics Video).mp3';
+import music10 from '../audio/NhacViet/NGÀY ĐẦU SAU CHIA TAY - ĐỨC PHÚC x THUỲ TIÊN x KHẮC HƯNG  OFFICIAL MUSIC VIDEO.mp3';
+import music11 from '../audio/NhacViet/Noo Phước Thịnh - Mãi Mãi Bên Nhau.mp3';
+import music12 from '../audio/NhacViet/Thêm Bao Nhiêu Lâu - Đạt G  OFFICIAL MV.mp3';
+import music13 from '../audio/NhacViet/THẰNG ĐIÊN  JUSTATEE x PHƯƠNG LY  OFFICIAL MV.mp3';
+import music14 from '../audio/NhacViet/Tìm Một Người Như Thế - Trungg I.U  OFFICIAL.mp3';
+import music15 from '../audio/NhacViet/Tùng TeA - Già Cùng Nhau Là Được ft. PC ( Prod. VoVanDuc. )  Official MV.mp3';
+import music16 from '../audio/NhacViet/Âm Thầm Bên Em.mp3';
+import music17 from '../audio/NhacViet/Đom Đóm - Jack [Lyrics video].mp3';
+import music18 from '../audio/NhacViet/suýt nữa thì.mp3';
+import music19 from '../audio/NhacViet/MIN - ĐỪNG YÊU NỮA, EM MỆT RỒI  OFFICIAL MUSIC VIDEO.mp3';
+import music20 from '../audio/NhacViet/CẢM GIÁC LÚC ẤY SẼ RA SAO  LOU HOÀNG  OFFICIAL AUDIO.mp3';
+import music21 from '../audio/NhacViet/Có ai thương em như anh.mp3';
+import music22 from '../audio/NhacViet/Có Chàng Trai Viết Lên Cây - Phan Mạnh Quỳnh  MẮT BIẾC OST.mp3';
+import music23 from '../audio/NhacViet/Wn  -  a b c d x y z n m a s a d (song 24)  3107.mp3';
+import music24 from '../audio/NhacViet/Kaidinh x @SIVAN1411  - Thế hệ tan vỡ  Official Lyric Video.mp3';
+import music25 from '../audio/NhacViet/Orange - Tự Sự ft. Thuận Nguyễn l Qua Bển Làm Chi OST - Phim đang chiếu tại rạp.mp3';
+import music26 from '../audio/NhacViet/Phùng Khánh Linh - Hôm Nay Tôi Buồn (Official Music Video).mp3';
+import music27 from '../audio/NhacViet/CÔ ẤY NÓI - NGÔ ANH ĐẠT  OFFICIAL LYRICS VIDEO.mp3';
+import music28 from '../audio/NhacViet/TELDAKTN.mp3';
+import music29 from '../audio/NhacViet/Thì Thôi - Reddy  MV Lyrics Official.mp3';
+import music30 from '../audio/NhacViet/SIVAN - haydeanhduoccungemdau ft. Kai Đinh (Official Music Video).mp3';
+import music31 from '../audio/NhacViet/24H - LyLy ft. Magazine.mp3';
+import music32 from '../audio/NhacViet/Giấc Mơ Của Em.mp3';
+import music33 from '../audio/NhacViet/Em Gái Mưa [ Lyric ]  Hương Tràm  ZymMii.mp3';
+import music34 from '../audio/NhacViet/Đừng Ai Nhắc Về Anh Ấy - Trà My Idol (Lyrics Video).mp3';
+import music35 from '../audio/NhacViet/ĐỢI - 52Hz (prod. RIO)  Official Lyric Video.mp3';
+import music36 from '../audio/NhacViet/tlinh - nếu lúc đó  OFFICIAL VISUALIZER.mp3';
+const allSongs = [music1, music2, music3, music4, music5,
+  music6, music7, music8, music9, music10, music11, music12,
+  music13, music14, music15, music16, music17, music18,music19,
+  music20, music21, music22, music23, music24, music25,
+  music26, music27, music28, music29, music30, music31,
+  music32, music33, music34, music35, music36];
+
+export default allSongs;
