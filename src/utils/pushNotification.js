9ff2bb7a1b60586c3c22886d1b27f67cabcfd1c6
Tecede <thoaixd123@gmail.com>
2025-09-04 23:59:10 +0700

    update

--- src/utils/pushNotification.js
+++ src/utils/pushNotification.js
@@ -1,33 +1,33 @@
 export async function subscribeUser() {
-  if (!("serviceWorker" in navigator)) return;
+  if ("serviceWorker" in navigator) {
+    try {
+      const register = await navigator.serviceWorker.register("/worker.js", { scope: "/" });
 
-  try {
-    const registration = await navigator.serviceWorker.ready; // lấy registration đã register trước đó
-    const existing = await registration.pushManager.getSubscription();
+      // kiểm tra nếu đã có subscription rồi thì không đăng ký lại
+      const existing = await register.pushManager.getSubscription();
+      if (existing) {
+        console.log("🔔 User đã đăng ký rồi, không cần đăng ký lại");
+        return;
+      }
 
-    if (existing) {
-      console.log("🔔 User đã đăng ký rồi");
-      return;
+      // nếu chưa có mới đăng ký
+      const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID_KEY),
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/api/songs/subscribe`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(subscription),
+      });
+
+      alert("✅ Đã đăng ký nhận thông báo!");
+    } catch (err) {
+      console.error("❌ Lỗi khi đăng ký push:", err);
     }
-
-    const subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID_KEY),
-    });
-
-    await fetch(`${process.env.REACT_APP_API_URL}/api/songs/subscribe`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(subscription),
-    });
-
-    alert("✅ Đã đăng ký nhận thông báo!");
-  } catch (err) {
-    console.error("❌ Lỗi khi đăng ký push:", err);
   }
 }
-
-
 
 // helper để convert VAPID key
 function urlBase64ToUint8Array(base64String) {
